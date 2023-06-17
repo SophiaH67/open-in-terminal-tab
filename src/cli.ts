14ec9b9900938d69892bdbafb6f8938dd4dcf517
marnixah <marnix@roboco.dev>
2023-06-17 19:53:46 +0200

    feat: implement the thing

--- src/cli.ts
+++ src/cli.ts
@@ -1,14 +1,44 @@
 import { Command } from 'commander';
+import { getCommand } from './getCommand';
+import spawn from 'cross-spawn';
 
-const packageJson = require('../package.json');
+import packageJson from '../package.json';
 const version: string = packageJson.version;
 
 const program = new Command();
 
 program
   .version(version)
-  .name('my-command')
-  .option('-d, --debug', 'enables verbose logging', false)
+  .name('open-in-terminal-tab')
+  .description('Execute a given command in a new terminal tab')
+  .arguments('<command>')
+  .option('-t, --title <title>', 'title of the new terminal tab', 'New Tab')
   .parse(process.argv);
 
-// Function code for CLI goes here
+const options = program.opts();
+
+const commandToRun = getCommand(program.args, options.title);
+
+const [command, ...args] = commandToRun;
+
+// Below is taken from cross-env
+// https://github.com/kentcdodds/cross-env/blob/3edefc7b450fe273655664f902fd03d9712177fe/src/index.js#L13-L36
+const proc = spawn(command, args, {
+  stdio: 'inherit',
+  shell: true,
+  env: process.env,
+});
+
+process.on('SIGTERM', () => proc.kill('SIGTERM'));
+process.on('SIGINT', () => proc.kill('SIGINT'));
+process.on('SIGBREAK', () => proc.kill('SIGBREAK'));
+process.on('SIGHUP', () => proc.kill('SIGHUP'));
+proc.on('exit', (code, signal) => {
+  let crossEnvExitCode = code;
+  // exit code could be null when OS kills the process(out of memory, etc) or due to node handling it
+  // but if the signal is SIGINT the user exited the process so we want exit code 0
+  if (crossEnvExitCode === null) {
+    crossEnvExitCode = signal === 'SIGINT' ? 0 : 1;
+  }
+  process.exit(crossEnvExitCode); //eslint-disable-line no-process-exit
+});
---
+++ src/getCommand.ts
@@ -0,0 +1,46 @@
+export class TerminalNotSupportedError extends Error {
+  constructor() {
+    super('Terminal not supported');
+  }
+}
+
+export function getCommand(
+  cmd: string[],
+  name: string,
+  returnCommandIfNoMatch = true
+): string[] {
+  const { TERM } = process.env;
+  const cwd = process.cwd();
+  const cmdString = cmd.join(' ');
+
+  if (TERM == 'xterm-kitty')
+    return [
+      'kitty',
+      '@',
+      'launch',
+      '--cwd',
+      cwd,
+      '--type=tab',
+      '--title',
+      name,
+      '--',
+      'bash',
+      '-c',
+      `npx ${cmdString}`,
+    ];
+
+  if (process.env.GNOME_TERMINAL_SCREEN || process.env.GNOME_TERMINAL_SERVICE)
+    return [
+      'gnome-terminal',
+      '--tab',
+      `--title=${name}`,
+      '--',
+      'bash',
+      '-c',
+      `npx ${cmdString}`,
+    ];
+
+  if (returnCommandIfNoMatch) return cmd;
+
+  throw new TerminalNotSupportedError();
+}
